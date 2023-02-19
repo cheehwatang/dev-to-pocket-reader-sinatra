65c852f6a690bde99e9db8e7aac2dec0c1c19642
Chee Hwa Tang <cheehwa.tang@gmail.com>
2023-02-19 15:56:14 +0800

    Adds ::all to Author

--- lib/app.rb
+++ lib/app.rb
@@ -1,6 +1,8 @@
 require 'sinatra'
 require 'sqlite3'
 
+require_relative './models/author'
+
 DB = SQLite3::Database.new(File.join(File.dirname(__FILE__), 'db/pocket_reader.sqlite'))
 
 get '/' do
--- lib/models/author.rb
+++ lib/models/author.rb
@@ -7,4 +7,12 @@ class Author
     @posts_published = attributes[:posts_published] || 0
     @comments_written = attributes[:comments_written] || 0
   end
+
+  def self.all
+    query = <<-SQL
+      SELECT * FROM authors
+    SQL
+    DB.results_as_hash = true
+    DB.execute(query)
+  end
 end
