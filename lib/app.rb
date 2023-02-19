9dcc1f3fb6e8cbae6bc4ba05a39363be34e20768
Chee Hwa Tang <cheehwa.tang@gmail.com>
2023-02-19 17:09:33 +0800

    Adds ::all and ::find(id) to Post

--- lib/app.rb
+++ lib/app.rb
@@ -2,6 +2,7 @@ require 'sinatra'
 require 'sqlite3'
 
 require_relative './models/author'
+require_relative './models/post'
 
 DB = SQLite3::Database.new(File.join(File.dirname(__FILE__), 'db/pocket_reader.sqlite'))
 
--- lib/models/post.rb
+++ lib/models/post.rb
@@ -1,3 +1,5 @@
+require_relative './author'
+
 class Post
   def initialize(attributes = {})
     @id = attributes[:id] || 0
@@ -5,6 +7,28 @@ class Post
     @content = attributes[:content] || ''
     @path = attributes[:path] || ''
     @read = attributes[:read] || false
-    @author = nil
+    @author = attributes[:author]
+  end
+
+  def self.all
+    query = <<-SQL
+      SELECT * FROM posts
+    SQL
+    DB.results_as_hash = true
+    DB.execute(query).map do |attributes|
+      attributes['author'] = Author.find(attributes['author_id'])
+      new(attributes.transform_keys(&:to_sym))
+    end
+  end
+
+  def self.find(id)
+    query = <<-SQL
+      SELECT * FROM posts
+      WHERE posts.id = ?
+    SQL
+    DB.results_as_hash = true;
+    attributes = DB.execute(query, id).first
+    attributes['author'] = Author.find(attributes['author_id'])
+    new(attributes.transform_keys(&:to_sym))
   end
 end
